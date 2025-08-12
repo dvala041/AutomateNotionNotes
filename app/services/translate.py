5946203e67241c5ff26c37345a1f9dda499a6dea
dvala041 <86671843+dvala041@users.noreply.github.com>
2025-08-11 22:33:26 -0400

    very minor commit

--- app/services/translate.py
+++ app/services/translate.py
@@ -5,9 +5,10 @@ if __name__ == "__main__":
     audio_file= open("~/Downloads/extracted_audio/Video by productplaybook_.mp3", "rb")
 
     transcription = client.audio.transcriptions.create(
-        model="gpt-4o-transcribe", 
+        model="gpt-4o-transcribe",  
         file=audio_file,
         prompt="I am a 20 year old interested in personal finance, entrepreneurship, and self-improvement. I want to learn more about these topics. Please summarize the audio content in a way that is engaging and informative for someone with my interests."
+        #stream=True  # May prove helpful for showing real-time transcription updates
     )
 
     print(transcription.text)
